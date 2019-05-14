3921c8f2316012ea63e7265f35056c8b063cefeb
meryemsahinn <meryem.sahin@useinsider.com>
2019-05-14 15:56:01 +0300

    V1.1

--- sql.py
+++ sql.py
@@ -2,5 +2,8 @@ import sqlite3
 
 with sqlite3.connect("apidatabase.db") as connection:
     c = connection.cursor()
-   # c.execute("""DROP TABLE visiters""")
+    c.execute("""DROP TABLE visiters""") #bunu fonksiyona çevir
     c.execute("""CREATE TABLE visiters(name TEXT, surname TEXT)""")
+    c.close()
+
+    #connection kapat
